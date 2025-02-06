c7dec8133e17956094059cdc15733e6739937cf2
EhadKs <ehad2004@hotmail.de>
2025-02-06 12:35:56 +0100

    Logo richtig laden

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { LeistungenComponent } from './components/Leistungen/leistungen.component';
+import { LeistungenComponent } from './components/leistungen/leistungen.component';
 import { KontaktComponent } from './components/Kontakt/Kontakt.component';
 import { AboutComponent } from './components/about/about.component';
 import { ImpressumComponent } from './components/impressum/impressum.component';
