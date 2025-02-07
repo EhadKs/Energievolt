0453809932311309e5821d9c2640147c404d3891
EhadKs <ehad2004@hotmail.de>
2025-02-07 17:41:34 +0100

    Anklicken der Buttons leitet nicht auf startseite zurück

--- src/app/components/leistungen/leistungen.component.ts
+++ src/app/components/leistungen/leistungen.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-leistungen',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leistungen.component.html',
   styleUrls: [ './leistungen.component.css']
 })
 export class LeistungenComponent { 
   constructor() {
     console.log('LeistungenComponent geladen');
+  }
+  section: string = 'photovoltaik'; // Standardmäßig Abschnitt 1
+
+  changeSection(sectionName: string) {
+    this.section = sectionName;
   }
 
 
