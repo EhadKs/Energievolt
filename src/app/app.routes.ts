f2bfd4f20df72b5a87466aab5a36f53fffc52a1b
batu58 <56992621+batu58@users.noreply.github.com>
2025-02-09 21:01:14 +0100

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import { KontaktComponent } from './components/Kontakt/Kontakt.component';
 import { AboutComponent } from './components/about/about.component';
 import { ImpressumComponent } from './components/impressum/impressum.component';
 import { DatenschutzComponent } from './components/datenschutz/datenschutz.component';
+import { FAQComponent } from './components/faq/faq.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'Leistungen', component: LeistungenComponent},
+  { path: 'leistungen', component: LeistungenComponent},
   { path: 'kontakt', component: KontaktComponent },
   { path: 'about', component: AboutComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'datenschutz', component: DatenschutzComponent },
+  { path: 'faq', component: FAQComponent }
   
   
 ];
